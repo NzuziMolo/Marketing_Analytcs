aacee8b3b774be954637e10dd27cff38efc15290
NZUZI MOLO JOAO ARMINDO <nzuzi.armindo@molodata.com.br>
2022-02-07 18:59:51 -0300

    Finalizado o Projeto

--- Projeto_chocolate/Análise_Vendas.py
+++ Projeto_chocolate/Análise_Vendas.py
@@ -32,11 +32,11 @@ pd.set_option('float_format', '{:.2f}'.format)
 df_base = pd.read_excel(r'C:\Users\Nzuzi Armindo\Documents\Programacao_DC\Jupter_notebook\Marketing_Analytics\Projeto_chocolate\Chocolate.xlsx')
 
 
-# In[4]:
+# In[5]:
 
 
 ##informações do df:
-df_base.size
+display(df_base)
 
 
 # ## Resumo:
@@ -45,28 +45,28 @@ df_base.size
 #  - Média de vendas por Semana
 #  - Variação de Vendas por Semana
 
-# In[5]:
+# In[6]:
 
 
 # Colunas da tabela
 df_base.columns
 
 
-# In[6]:
+# In[7]:
 
 
 # Transformar todas as Colunas em menúsculas:
 df_base.columns = df_base.columns.str.lower()
 
 
-# In[7]:
+# In[8]:
 
 
 # Modificar o indice
 df_base.set_index('semana',inplace=True)
 
 
-# In[8]:
+# In[9]:
 
 
 ### Adicionar a coluna mercado que é a soma de todas as vendas:
@@ -77,7 +77,7 @@ for i in df_base.columns:
         
 
 
-# In[9]:
+# In[10]:
 
 
 df_base.head(5)
@@ -86,14 +86,14 @@ df_base.head(5)
 # 
 # ## Total de Mercado
 
-# In[10]:
+# In[11]:
 
 
 mercado = df_base['mercado'].sum()
 print(f'Total de mercado de Chocolate foi de: R${mercado}')
 
 
-# In[11]:
+# In[12]:
 
 
 ## Análise de mercado ao longo do tempo:
@@ -113,14 +113,14 @@ fig.update_layout(
 fig.show()
 
 
-# In[12]:
+# In[14]:
 
 
 # análise estatistica do mercado:
 data.describe()
 
 
-# In[13]:
+# In[17]:
 
 
 # Histrograma de total de mercado:
@@ -128,13 +128,14 @@ df= data
 fig = figsize=(9,6)
 fig = px.histogram(df, 
                    marginal='violin',
-                   template='simple_white'
+                   template='simple_white',
+                   labels={'value':'Mercado'}
                   )
-fig.update_layout(title={'text':'Histograma de Mercado', 'y':0.95, 'x':0.5})
+fig.update_layout(title={'text':'Histograma do Mercado', 'y':0.95, 'x':0.5})
 fig.show()
 
 
-# In[14]:
+# In[18]:
 
 
 ## Boxplot do total de mercado:
@@ -142,7 +143,7 @@ fig= figsize=(10,6)
 fig = px.box(data, x='mercado',
              labels={'value':'Vendas','variable': ''},points='all',template='simple_white')
 fig.update_layout(title={
-    'text': 'Boxplot de Mercado de Chocolate',
+    'text': 'Boxplot de Mercado',
     'y': 0.95,
     'x': 0.5
 })
@@ -153,7 +154,7 @@ fig.show()
 #  
 #  verificando a variação do mercado ao longo do tempo:
 
-# In[15]:
+# In[73]:
 
 
 df_variacao_mercado = data.pct_change()
@@ -161,63 +162,78 @@ df_variacao_mercado = data.pct_change()
 df_variacao_mercado
 
 
-# In[16]:
-
-
-# Variação de Mercado:
-variacao = df_variacao_mercado
-fig = figsize=(9, 6)
-fig = px.line(variacao, y='mercado',
-              labels={'index': 'Semana', 'mercado': 'Variação de Mercado%'},
-              template='simple_white'
-             )
-fig.update_layout(title={'text':'Variação de Mercado', 'y':0.95, 'x':0.5})
-             
-fig.show()
-
-
-# In[17]:
-
-
-# Variação de Mercado dos ultimos 4 semanas:
-variacao = df_variacao_mercado['mercado'].rolling(4).mean()
-fig = figsize=(9, 6)
-fig = px.line(variacao, y='mercado',
-              labels={'index': 'Semana', 'mercado': 'Variação de Mercado %'},
-              template='simple_white'
-             )
-fig.update_layout(title={'text':'Variação Movel de Mercado das ultimas 4 semana', 'y':0.95, 'x':0.5})
-             
-fig.show()
-
-
-# In[18]:
+# In[74]:
 
 
 # Taxa de Crescimento - calculando a média variação total:
 taxa_crecimento = df_variacao_mercado['mercado'].mean()
-print(f'A taxa de crescimento do mercado é de {"%.2f"%taxa_crecimento}%')
+print(f'A taxa de crescimento médio do mercado é de {"%.2f"%taxa_crecimento}%')
 
 
-# In[19]:
+# In[75]:
+
+
+# Média da taxa de crescimento movel do mercado das ultimas 4 semanas: 
+df_variacao_mercado['mercado_movel']= df_variacao_mercado['mercado'].rolling(4).mean() 
+
+
+# In[76]:
 
 
 # Variação de Mercado de Chocolates
-df_variacao_mercado['taxa_crecimento'] = taxa_crecimento
+df_variacao_mercado['taxa_média_crecimento'] = taxa_crecimento
 
 
-# In[20]:
+# In[77]:
+
+
+df_variacao_mercado
+
+
+# In[78]:
+
+
+# Variação de Mercado:
+variacao = df_variacao_mercado['mercado']
+fig = figsize=(9, 6)
+fig = px.line(variacao, y='mercado',
+              labels={'index': 'Semana', 'mercado': 'Variação de Mercado'},
+              template='simple_white'
+             )
+fig.update_layout(title={'text':'Variação Semanal do Mercado', 'y':0.95, 'x':0.5})
+             
+fig.show()
+
+
+# In[79]:
+
+
+# Variação de Mercado dos ultimos 4 semanas:
+variacao = df_variacao_mercado[['mercado','mercado_movel']]
+variacao.columns=['Variação de Mercado', 'Variaçao Movel 4 semanas']
+fig = figsize=(9, 6)
+fig = px.line(variacao,
+              labels={'index': 'Semana', 'mercado': 'Variação de Mercado '},
+              template='simple_white'
+             )
+fig.update_layout(title={'text':'Média Movel de Avalizção de Mercado', 'y':0.95, 'x':0.5})
+             
+fig.show()
+
+
+# In[80]:
 
 
 # Variação de Mercado:
 df = df_variacao_mercado
+df.columns=['Variação de Mercado', 'Variaçao Movel 4 semanas','Taxa de Crecimento']
 fig = figsize=(9, 6)
 fig = px.line(df,
-              labels={'index': 'Semana', 'mercado': 'Variação de Mercado',
-                      'taxa_crecimento':'Taxa de Crescimento'},
+              labels={'index': 'Semana', 'value': 'Variação de Mercado',
+                      },
               template='simple_white')
 fig.update_layout(title={
-                    'text': 'Variação do Mercado',
+                    'text': 'Taxa de Crescimento de Mercado',
                     'y':0.95,
                     'x':0.5}
                  )             
@@ -227,16 +243,14 @@ fig.show()
 # # Análise de Produtos:
 # 
 
-# In[21]:
+# In[81]:
 
 
-vendas = []
-for c in list(df_base.columns):
-    if c.startswith('vendas'):
-        vendas.append(c)
+## Criando uma lista com as colunas de vendas:
+vendas = [c for c in df_base.columns if c.startswith('vendas')]
 
 
-# In[22]:
+# In[82]:
 
 
 # Filtrar a tabela somente com os valores de vendas:
@@ -245,75 +259,67 @@ vendas_marcas= df_base[vendas].sum().sort_values(ascending=False)
 vendas_marcas.index = vendas_marcas.index.str.replace('vendas_',"").str.strip().str.title()
 
 
-# In[23]:
+# In[83]:
 
 
 df = vendas_marcas
 fig = px.bar(vendas_marcas, 
              template='simple_white', 
              text='value',
-            labels={'index': 'produto', 'value': 'Mercado'}
+            labels={'index': 'Marca', 'value': 'Vendas'}
             )
 fig.update_traces(textposition='inside',
                   texttemplate='%{text:.2s}'
                  )
-fig.update_layout(title={'text':'Total  de Mercado por Produto','y':0.95,'x':0.5})
+fig.update_layout(title={'text':'Total  de Mercado por Marca','y':0.95,'x':0.5})
 fig.show()
 
 
 # - Participação Total de Mercado por Produto
 
-# In[24]:
+# In[84]:
 
 
 # Participação Total de Mercado
 df = vendas_marcas/mercado*100
 fig = px.bar(df, template='simple_white', text='value',
-            labels={'index': 'produto', 'value': ' Total Participação %'})
+            labels={'index': 'Mercado', 'value': ' Total Participação %'})
 fig.update_traces(textposition='inside',texttemplate='%{text:.2}%')
-fig.update_layout(title={'text':'Participaçãp Total de Mercado','y':0.95,'x':0.5})
+fig.update_layout(title={'text':'Participação Total de Mercado','y':0.95,'x':0.5})
 fig.show()
 
 
 # ## Análise detalhadas:
 # - Média de Mercado por Marca
 
-# In[25]:
+# In[85]:
 
 
 df_base[vendas].describe()
 
 
-# In[26]:
+# In[86]:
 
 
 ##Boxplot de Vendas:
 df= df_base[vendas]
+df.columns= df.columns.str.replace('vendas_','').str.title()
 fig = px.box(df, 
              template='simple_white',
-             labels={'value':'Mercado','variable': ''}
+             labels={'value':'Vendas','variable': ''}
             )
 fig.update_layout(title={'text':'Boxplot de Vendas por Marca','y':0.95,'x':0.5})
 fig.show()
 
 
-# In[27]:
-
-
-#Adicionando os nomes das colunas para o filtro:
-vendas2= vendas
-vendas2
-
-
-# In[28]:
+# In[87]:
 
 
 # Adicionando a coluna mercado
-vendas2.append('mercado')
-vendas2
+vendas2=[c for c in df_base.columns if c.startswith('vendas') or c == 'mercado']
 
 
-# In[29]:
+# In[88]:
 
 
 # Criando um DataFrame df_vendas2 para seleciornar-mos as colunas necessaria para a nossa analise:
@@ -322,7 +328,7 @@ df_vendas2= df_base[vendas2]
 
 # - Calculando a participação de mercado para cada marca por semana
 
-# In[30]:
+# In[89]:
 
 
 for i in df_vendas2.columns:
@@ -331,14 +337,20 @@ for i in df_vendas2.columns:
          
 
 
-# In[31]:
+# In[90]:
 
 
 # Retirando a coluna mercado da amostra:
 df_vendas2= df_vendas2.drop(['mercado'], axis=1)
 
 
-# In[32]:
+# In[93]:
+
+
+df_vendas2.columns= df_vendas2.columns.str.replace('Vendas_','').str.strip().str.title()
+
+
+# In[94]:
 
 
 df_vendas2.head(5)
@@ -347,21 +359,14 @@ df_vendas2.head(5)
 # - Análise descritiva das Marcas:
 #  
 
-# In[34]:
+# In[95]:
 
 
 # Resumo estatístico da participação de mercado  
 df_vendas2.describe()
 
 
-# In[35]:
-
-
-# Renomendo as colunas:
-df_vendas2.columns = df_vendas2.columns.str.replace('Vendas','Participacao').str.strip().str.title()
-
-
-# In[36]:
+# In[96]:
 
 
 # Boxplot da participação de mercado:
@@ -369,9 +374,9 @@ df= df_vendas2
 fig = px.box(df, 
              template='simple_white',
              points='all',
-             labels={'value':' de Participação','variable': ''}
+             labels={'value':' Participação do Mercado','variable': 'Marca'}
             )
-fig.update_layout(title={'text':'Participação por Marca','y':0.95,'x':0.5})
+fig.update_layout(title={'text':'Boxplot de Participação por Marca','y':0.95,'x':0.5})
 fig.show()
 
 
@@ -379,24 +384,24 @@ fig.show()
 
 # A Marca Whittaker apresenta maior Variabilidade:
 
-# In[37]:
+# In[75]:
 
 
 ## Histograma da participação da Whittaker:
-df=df_vendas2['Participacao_Whittaker']
+df=df_vendas2['Whittaker']
 fig = figsize=(9,6)
 fig = px.histogram(df, 
                    template='simple_white',
-                   labels={'value': ' Participação'}
+                   labels={'value': ' Participação', 'count':'nº de partipação'}
                   )
 fig.update_layout(title={'text':'Histograma de Mercado', 'y':0.95, 'x':0.5})
 fig.show()
 
 
-# ### Para Obter maior detalhes Vamos dividir a base sem 2 anos para entedermos as mudanças de participação das marcas
+# #### Para Obter maior detalhes Vamos dividir a base em 2 anos para entedermos as mudanças de participação das marcas
 # 
 
-# In[38]:
+# In[97]:
 
 
 # A função personalizada
@@ -405,14 +410,14 @@ def ano(Coluna):
     return x
 
 
-# In[39]:
+# In[98]:
 
 
 # Adicionando a coluna de Ano:
 df_base['ano']= ano(df_base.index)
 
 
-# In[40]:
+# In[99]:
 
 
 df_vendas2['ano']= ano(df_vendas2.index)
@@ -420,26 +425,27 @@ df_vendas2['ano']= ano(df_vendas2.index)
 
 # - Participação de mercado das marcas por ano
 
-# In[41]:
+# In[100]:
 
 
-# Resumo do 1º ano:
+# Resumo de participação de mercado do 1º ano:
 df_vendas2[df_vendas2['ano']=='1º ano'].describe()
 
 
-# In[42]:
+# In[101]:
 
 
+#Resumo de participação de mercado do 2º ano
 df_vendas2[df_vendas2['ano']=='2º ano'].describe()
 
 
-# In[43]:
+# In[102]:
 
 
 df_vendas2.columns
 
 
-# In[44]:
+# In[103]:
 
 
 df = df_vendas2.groupby('ano').agg(lambda x: np.mean(x)*100).T
@@ -451,7 +457,7 @@ fig.update_layout(title={'text':'Percentual Médio de Participação de Mercado'
 fig.show()
 
 
-# In[45]:
+# In[104]:
 
 
 
@@ -467,13 +473,13 @@ fig.show()
 
 # - Analizar a marca whittaker e a Caddury
 
-# In[46]:
+# In[105]:
 
 
 df_base.columns
 
 
-# In[47]:
+# In[106]:
 
 
 # Gráfico de disperção da Marca Whitteker
@@ -483,7 +489,7 @@ fig.update_layout(title={'text':'Vendas x Preço da Whittaker','y':0.95,'x':0.5}
 fig.show()
 
 
-# In[48]:
+# In[107]:
 
 
 # Gráfico de disperção da Marca Whitteker
@@ -495,24 +501,61 @@ fig.update_layout(title={'text':'Vendas x Preço da cadbury','y':0.95,'x':0.5})
 fig.show()
 
 
-# - Aplicando o logaritimo natural:
-# 
+# - Estratégia de preço
 
-# In[49]:
+# In[108]:
 
 
-# Gráfico de disperção da Marca Whitteker
-df= df_base[['vendas_whittaker', 'preco_atual_whittaker']]
-fig= px.scatter(df, x='preco_atual_whittaker', y='vendas_whittaker', 
-                trendline='ols',
-                template="simple_white",
-               log_x=True, log_y=False)
-fig.update_layout(title={'text':'Vendas x Preço da Whittaker','y':0.95,'x':0.5})
+### Preço Médio das Marcas
+preco = [i for i in df_base.columns if i.startswith('preco_atual') or i =='ano']
+
+
+# In[109]:
+
+
+# Estrategia de Preco
+df_base[preco].groupby('ano').mean().T
+
+
+# In[110]:
+
+
+df= df_base[preco].groupby(ano).mean().T
+df.index =df.index.str.replace('preco_atual_','').str.strip().str.title()
+fig= px.bar(df,barmode="group",text='value',
+           labels={'value':'Média de Preço', 'index':'Marca'},
+            template='simple_white'
+           )
+fig.update_traces(textposition='inside',texttemplate='%{text:.2}')
+fig.update_layout(title={'text':'Média de Preço por Marca','y':0.95,'x':0.5})
 fig.show()
 
 
-# In[ ]:
+#    - Estrategia de A&D
+
+# In[111]:
 
 
+## criando uma lista com somente as colunas de anuncios.
+anunciodisplay = [c for c in df_base.columns if c.startswith('ad_') or c == 'ano']
 
+
+# In[112]:
+
+
+df_base[anunciodisplay].groupby('ano').agg(lambda x: np.mean(x)*100).T
+
+
+# In[113]:
+
+
+df = df_base[anunciodisplay].groupby('ano').agg(lambda x: np.mean(x)*100).T
+df.index = df.index.str.replace('ad_', '').str.strip().str.title()
+fig= px.bar(df,barmode="group",text='value',
+           labels={'value':'Média de Preço', 'index':'Marca'},
+            template='simple_white'
+           )
+fig.update_traces(textposition='inside',texttemplate='%{text:.2}')
+fig.update_layout(title={'text':'Média de Preço por Marca','y':0.95,'x':0.5})
+fig.show()
 
